cdb686c85b2623dfae2b509c7421ccfad64f4ef5
Chen-Oliver <oliver@Olivers-MBP.home>
2017-07-29 22:27:50 -0400

    .

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def hello():
     return render_template("index.html",start=startPage,curr=currPage,end=endPage)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
