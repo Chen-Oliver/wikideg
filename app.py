23eb3ddca398b96c3148fd8f845672b883720e61
Chen-Oliver <oliver@Olivers-MBP.home>
2017-08-07 16:47:41 -0400

    uncomment stuff

--- app.py
+++ app.py
@@ -39,6 +39,5 @@ def index():
     return render_template("index.html",start=startPage,curr=currPage,end=endPage)
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
